09deabbe350b611365f8a7cac6ce28498ced53cc
herb nadal <harrisonnad64@gmail.com>
2020-12-19 13:27:17 -0500

    Add animation and rolling feature

--- src/Die.js
+++ src/Die.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react'
+import './Die.css'
 
 export default class Die extends Component {
-    static defaultProps = {
-        dice =[
-            { id: 1, side: 'fas fa-dice-one' },
-            { id: 2, side: 'fas fa-dice-two' },
-            { id: 3, side: 'fas fa-dice-three' },
-            { id: 4, side: 'fas fa-dice-four' },
-            { id: 5, side: 'fas fa-dice-five' },
-            { id: 6, side: 'fas fa-dice-six' },
-        ]
-    }
     constructor(props) {
         super(props)
     
@@ -18,15 +9,11 @@ export default class Die extends Component {
              number: 6,
         }
     }
-    rollDice() {
-        let rand = Math.floor(Math.random * this.number);
 
-
-    }
     render() {
         return (
             <div>
-                <i class={dice.}></i>
+                <i className={`Die fas fa-dice-${this.props.face} ${this.props.rolling && 'animate'}`}></i>
             </div>
         )
     }
